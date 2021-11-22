fa211f6476315277684af26a141c3e12f863f73c
zombic365 <zombic365@gmail.com>
2021-11-22 13:58:49 +0900

    test2

--- check_security.sh
+++ check_security.sh
@@ -6,19 +6,27 @@ source ./common.color.cfg
 U01(){
   RUNC "grep -n "/lib/security/pam_securetty.so" /etc/pam.d/login |cut -d: -f1"
   if [ "${res}" == 0 ]; then
-    line_num=$(grep -n "/lib/security/pam_securetty.so" /etc/pam.d/login |cut -d: -f1)
-    RUNC "sed -i '${line_num}s/.*/#&/g' /etc/pam.d/login"
+    RUNC "echo -e "${Check_No} ${BOLD}[U01] 'grep -n "/lib/security/pam_securetty.so" /etc/pam.d/login |cut -d: -f1'${Color_off}""
+  else
+    RUNC "echo -e "${Check_Yes} ${BOLD}[U01] 'grep -n "/lib/security/pam_securetty.so" /etc/pam.d/login |cut -d: -f1'${Color_off}""
   fi
+
   RUNC "find /etc -type f -name "securetty""
   if [ "${res}" == 0 ]; then
     RUNC "grep "pts" /etc/securetty"
     if [ "${res}" == 0 ]; then
-      RUNC "sed -i 's/pts/#&/g' /etc/securetty"
+      RUNC "echo -e "${Check_Yes} ${BOLD}[U01] 'grep "pts" /etc/securetty'""
+    else
+      RUNC "echo -e "${Check_No} ${BOLD}[U01] 'grep "pts" /etc/securetty'""
     fi
   fi
   RUNC "grep -i "PermitRootLogin yes" /etc/ssh/sshd_config"
   if [ "${res}" == 0 ]; then
-    RUNC "echo "[CHECK] [U01] []
+    RUNC "echo -e "${Check_No} ${BOLD}[U01] 'grep -i "PermitRootLogin yes" /etc/ssh/sshd_config'${Color_off}""
+  else
+    RUNC "echo -e "${Check_Yes} ${BOLD}[U01] 'grep -i "PermitRootLogin yes" /etc/ssh/sshd_config'${Color_off}""
+  fi
+}
 
 main(){
   U01
